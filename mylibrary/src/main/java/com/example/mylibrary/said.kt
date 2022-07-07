e13edfd0c5bea4933fef45fe28d777133cf000a2
kama_001 <krahmatullayev69@gmail.com>
2022-07-06 18:01:01 -1200

    77

--- mylibrary/src/main/java/com/example/mylibrary/said.kt
+++ mylibrary/src/main/java/com/example/mylibrary/said.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 object said {
-    
+
     fun show(context: Context) {
         Toast.makeText(context,"Hello Youtubes.",Toast.LENGTH_SHORT).show()
     }
